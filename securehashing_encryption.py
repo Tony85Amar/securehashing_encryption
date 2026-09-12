e5a5c680fc4cb2aa40b563b7a18040e7c57a8965
Tony85Amar <129993451+Tony85Amar@users.noreply.github.com>
2026-09-11 20:36:32 -0500

    Add files via upload

---
+++ securehashing_encryption.py
@@ -0,0 +1,45 @@
+
+SDEV245-10P-IO-202620-I-81X
+Module 3: Assignment - Secure Hashing and Encryption
+
+Module 3: Assignment - Secure Hashing and Encryption
+9/11/2026
+50 Points Possible
+Attempt 1
+In Progress
+NEXT UP: Submit Assignment
+
+Unlimited Attempts Allowed
+Get Ready
+Students will build a simple application with user login and role-based access control. This demonstrates the implementation of basic authentication and access restrictions based on role.
+
+This assignment will support the following outcomes:
+
+3.1 Describe how randomness affects cryptographic algorithm strength. 
+4.1 Encrypt a message using a simple substitution cipher
+4.2 Implement a SHA-256 hash to verify file integrity. 
+4.3 Illustrate the process of digital signatures using private/public key pairs.
+Supportive Materials
+To be successful with this assignment, you must complete each of the materials listed on the Learning Materials page. 
+
+The following resources are meant as guides for the assignment tasks listed below:
+
+Youtube: Python Beginner Project: Build a Caesar Cipher Encryption App [length 25:09]
+Webpage: How to use OpenSSL: Hashes, digital signatures, and more
+ 
+
+Complete Your Work
+Instructions:
+Choose a language (Python, Java, C/C++, or any language of your choice).
+
+Write an app that generates SHA-256 hashes for input strings or files
+
+Write an app that uses a simple substitution cipher (Caesar cipher or similar) to encrypt/decrypt input text
+
+Use OpenSSL or a tool to simulate a digital signature (sign/verify).
+Include a short README explaining your code's functionality
+
+Deliverables:
+Code files (GitHub link)
+
+README (brief explanation of your code's functionality)
